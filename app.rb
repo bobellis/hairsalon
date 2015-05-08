722ee8a2427917d9caa5863a25b0bb22214dc2d9
bob <bobellis.tx@gmail.com>
2015-05-08 14:55:47 -0700

    Add clients and stylists pages and routes to get and post methods for both

--- app.rb
+++ app.rb
@@ -13,3 +13,29 @@ get("/") do
   @clients = Client.all()
   erb(:index)
 end
+
+get("/stylists") do
+  @stylists = Stylist.all()
+  erb(:stylists)
+end
+
+post("/stylists") do
+  stylist_name = params.fetch("stylist_name")
+  new_stylist = Stylist.new({:name => stylist_name, :id => nil})
+  new_stylist.save()
+  @stylists = Stylist.all()
+  erb(:stylists)
+end
+
+get("/clients") do
+  @clients = Client.all()
+  erb(:clients)
+end
+
+post("/clients") do
+  client_name = params.fetch("client_name")
+  new_client = Client.new({:name => client_name, :id => nil})
+  new_client.save()
+  @clients = Client.all()
+  erb(:clients)
+end
